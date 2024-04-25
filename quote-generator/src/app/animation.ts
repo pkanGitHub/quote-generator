ec1b0713f0f12e209fd55ccb7e79b05249b14b9f
Phyllis Kan <pyk9q7@gmail.com>
2024-04-25 18:31:43 -0500

    Fade in finally work with stagger

--- quote-generator/src/app/animation.ts
+++ quote-generator/src/app/animation.ts
@@ -1,9 +1,20 @@
-import { transition, trigger, animate, style, stagger } from '@angular/animations';
+import { transition, trigger, animate, style, stagger, query } from '@angular/animations';
+
+// export let fadeIn = trigger('fadeIn', [
+//     // change to 'void <=> *' if we want fade out
+//     transition('void => *', [
+//         style({ opacity: 0, transform: 'translateX(-100px)' }),
+//         animate('2000ms ease-in'), // transition timing in millisecond
+//     ]),
+// ])
 
 export let fadeIn = trigger('fadeIn', [
-    // change to 'void <=> *' if we want fade out
-    transition('void => *', [
-        style({ opacity: 0 }),
-        animate('2000ms ease-in'), // transition timing in millisecond
+    transition(':enter', [
+        query(':enter', [
+            style({ opacity: 0, transform: 'translateX(-100px)'}),
+            stagger(200, [
+              animate('1000ms ease-in', style({ opacity: 1, transform: 'translateX(0px)'})),
+            ]),
+        ], { optional: true })
     ]),
 ])

ff07fedae92b9a7ee25c61d8d27336a6bb64da2f
Not_Shultz <59833741+MichaelCSturm@users.noreply.github.com>
2024-04-25 19:57:35 -0500

    LETS GOOO

--- quote-generator/src/app/quote-list/quote-list.component.ts
+++ quote-generator/src/app/quote-list/quote-list.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
-
+import { inject } from '@angular/core';
+import { QuoteGetterServicee } from 'app/quote-getter.service';
 @Component({
   selector: 'app-quote-list',
   templateUrl: './quote-list.component.html',
   styleUrl: './quote-list.component.css'
 })
 export class QuoteListComponent {
-
+  private quoteGetter = inject(QuoteGetterServicee)
+  msg;
+  loadPosts(){
+    this.quoteGetter.getPosts().subscribe({
+      next: (posts : string) => {
+       console.log()
+       
+       //const realposts = JSON.stringify(posts[0][0])
+       const Postquote =JSON.stringify(posts[0][0])
+       const turnback = JSON.parse(Postquote)
+       
+       
+        this.msg = turnback.quote + " Author:" + turnback.author + " Topic:" + turnback.topic
+        
+        
+      },
+     error: (error) => console.log('Error fetching posts', error)
+    })
+  }
 }

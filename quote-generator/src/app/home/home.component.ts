1fbb9f412c4e4a3ffe20ea9d86e4418501b4e67e
mbowman21 <sstraberry101@gmail.com>
2024-04-25 19:25:29 -0500

    made button work

--- quote-generator/src/app/home/home.component.ts
+++ quote-generator/src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  constructor (public router: Router) {}
 
+  genQuote() {
+    this.router.navigate(['/generate-quote'])
+  }
 }

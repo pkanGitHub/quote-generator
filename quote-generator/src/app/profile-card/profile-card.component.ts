7438582247f69ddc2a8aaef18962c2baac18f754
Stella <stellarstellacraig@gmail.com>
2024-04-29 21:09:02 -0500

    Update profile-card.component.ts

--- quote-generator/src/app/profile-card/profile-card.component.ts
+++ quote-generator/src/app/profile-card/profile-card.component.ts
@@ -11,19 +11,19 @@ import { fadeIn } from 'app/animation';
 })
 export class ProfileCardComponent {
   members = [
-    { 
-      name: 'Maya Bowman', 
-      imageUrl: 'assets/Maya.jpg', 
-      bio: 'is a Junior majoring in Computer Science at Mizzou. They work part-time as a data engineer. In their free time they like to play video games and are currently playing Subnautica. Her goal is to graduate with good grades' 
+    {
+      name: 'Maya Bowman',
+      imageUrl: 'assets/Maya.jpg',
+      bio: 'is a Junior majoring in Computer Science at Mizzou. They work part-time as a data engineer. In their free time they like to play video games and are currently playing Subnautica. Their goal is to graduate with good grades'
     },
-    { 
-      name: 'Stella Craig', 
-      imageUrl: 'assets/Stella.jpg', 
-      bio: 'is a Junior in Computer Science at Mizzou. They are also the Secretary of MCA and the head of marketing for TigerHacks. They love to play video games and read in their free time. Her goal is to one day own their own indie game development company.' 
+    {
+      name: 'Stella Craig',
+      imageUrl: 'assets/Stella.jpg',
+      bio: 'is a Junior in Computer Science at Mizzou. They are also the Secretary of MCA and the head of marketing for TigerHacks. They love to play video games and read in their free time. Their goal is to one day own their own indie game development company.'
     },
-    { 
-      name: 'Phyllis Kan', 
-      imageUrl: 'assets/Phyllis.jpg', 
+    {
+      name: 'Phyllis Kan',
+      imageUrl: 'assets/Phyllis.jpg',
       bio: 'is a senior in Information Technology at University of Missouri. She graduated from a coding bootcamp for software engineering prior going to Mizzou. She recently find interest in cyber security and looking to learn more about it. Her goal is to live in a stable place with her furry friends and works at the place she enjoys.'
     },
     { name: 'Michael Sturm', imageUrl: 'assets/Michael.png', bio: 'is in Information Technology major at Mizzou. He is interest in Nicehe programming topics' },
